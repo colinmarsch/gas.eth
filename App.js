5ed0e6fc43134c217cd9764afcd86032a70ddd5e
Colin Marsch <colinmarsch@gmail.com>
2021-12-02 15:16:44 -0500

    Add a simple VStack impl showing 3 blocks

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { NativeBaseProvider, Box } from 'native-base';
+import { VStack, Center, Heading, NativeBaseProvider } from "native-base"
+
+export function GasPrice() {
+	return (
+		<VStack space={4} alignItems="center">
+			<Heading textAlign="center" mb="10" mt="10">
+				Gas Price
+			</Heading>
+			<Center w="72" h="40" bg="primary.500" rounded="lg" shadow={3} />
+			<Center w="72" h="40" bg="secondary.500" rounded="lg" shadow={3} />
+			<Center w="72" h="40" bg="emerald.500" rounded="lg" shadow={3} />
+		</VStack>
+	)
+}
 
 function App() {
-  return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
-    </NativeBaseProvider>
-  );
+	return (
+		<NativeBaseProvider>
+			<GasPrice />
+		</NativeBaseProvider>
+	);
 }
 
 export default App;
